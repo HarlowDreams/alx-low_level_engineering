679f88be228365814d4b8487aa433cbe17d25354
HarlowDreams <idamajeff@gmail.com>
2022-07-06 12:54:41 -0700

    1 nest

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar('n');
+	}
+
+	_putchar('\n');
+	return (0);
+}
+
