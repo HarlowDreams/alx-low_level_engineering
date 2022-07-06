f2d3403fb3bb23d1ad8937ed7cebeeb2a3591e3a
HarlowDreams <idamajeff@gmail.com>
2022-07-06 15:23:19 -0700

    2-nest

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	int n;
 	int i = 0;
+	int n;
 
 	while (i < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
+		{
 			_putchar(n);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	i++;
 }
