f4d59c3f35a5965d656670aae2a64eb7f865f1e0
HarlowDreams <idamajeff@gmail.com>
2022-07-06 16:17:33 -0700

    3-nest

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - function that checks for a lowercase alphabet
- *
+ *@c: single letter character
  * Return: returns 0 and 1 depending on the condition
  */
 int _islower(int c)
