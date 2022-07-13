7eae21e3a3c966875c3b4dc06b70ddc7f045febf
HarlowDreams <idamajeff@gmail.com>
2022-07-13 03:48:00 -0700

    1 point

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - given two integers swap the values they are holding
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	a = 7 b = 8;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
