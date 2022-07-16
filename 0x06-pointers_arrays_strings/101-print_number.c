6ca1a9077386a004e61baecb170a2ce07f32ae87
HarlowDreams <idamajeff@gmail.com>
2022-07-15 19:10:28 -0700

    9 pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,19 +3,20 @@
 /**
  * print_number - prints an integer
  * @n: integer to be printed
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -m;
+		x = -x;
 	}
 	
-	if ((num / 10) > 0)
-		_putchar_number(num / 10);
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-	_putchar ((num % 10) + '\0');
+	_putchar (x % 10 + '0');
 }	
